6b1485d20482e9e2bbb9d7015b3fddc09d913a0a
shenxin <wb_shenxin@meituan.com>
2025-04-23 16:29:57 +0800

    feat: 增强内容脚本功能 - 添加了对特定元素的删除操作 - 创建了样式元素并注入自定义样式 - 更新了 footer 和其他特定类的处理逻辑

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -50,14 +50,62 @@ const xxxx = () => {
   document.querySelectorAll('.woo-button-main').forEach((side) => {
     side.remove();
   });
-}
+  // Frame_wrap_  背景红色
+  const appElement = document.querySelector('#app');
+  if (appElement) {
+    const frameWrapElement = appElement.querySelector('[class*="Frame_wrap"]');
+    if (frameWrapElement) {
+      (frameWrapElement as HTMLElement).classList.add('xxxxxx');
+    }
+  }
+  // woo-avatar-main woo-avatar-hover head_avatar_20c9y
+  document.querySelectorAll('.woo-avatar-main.woo-avatar-hover[class*="head_avatar_"]').forEach((side) => {
+    side.remove();
+  });
+  // class="card-vote_wrap_15NVR card-vote_original_2JeMZ content_row_-r5Tk"
+  document.querySelectorAll('[class*="card-vote_wrap_"][class*="card-vote_original_"][class*="content_row_"]').forEach((side) => {
+    side.remove();
+  });
+  // wbpro-feed-content
+  const contentElement = document.querySelectorAll('.wbpro-feed-content');
+  if (contentElement) {
+    contentElement.forEach((side) => {
+      (side as HTMLElement).style.paddingLeft = '0';
+    });
+  }
 
+  // footer
+  document.querySelectorAll('footer').forEach((side) => {
+    side.remove();
+  });
+
+  // class="woo-icon-wrap woo-icon-opacity IconVip_icon_2tjdp"
+  document.querySelectorAll('[class*="woo-icon-wrap"][class*="woo-icon-opacity"][class*="IconVip_icon_"]').forEach((side) => {
+    side.remove();
+  });
+}
 
 export default defineContentScript({
   matches: ['*://*.weibo.com/*'],
   cssInjectionMode: 'ui',
   async main(ctx) {
-    console.log('222', 222)
+    // 创建 style 元素
+    const style = document.createElement('style');
+    style.textContent = `
+      .xxxxxx {
+        background-color: #e8e8e8 !important;
+      }
+      
+      a{
+      color: #000 !important;
+      }
+       
+    `;
+
+    // 将 style 元素添加到文档头部
+    document.head.appendChild(style);
+
+    console.log('222', 222);
     // 创建 MutationObserver 实例
     const observer = new MutationObserver((mutations) => {
       xxxx();
