f07bb6f629758390cc006c45dde24b7dcf791cc0
shenxin <wb_shenxin@meituan.com>
2025-04-23 15:58:07 +0800

    feat: 添加内容脚本功能 - 实现了删除特定标签和类的功能 - 更新了匹配的 URL 为 weibo.com - 添加了浮动按钮，点击后触发删除操作

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,6 +1,80 @@
+import './style.css';
+
+const xxxx = () => {
+  // 删除所有的<video>标签
+  document.querySelectorAll('video').forEach((video) => {
+    video.remove();
+  });
+  // 删除所有的<svg>标签
+  document.querySelectorAll('svg').forEach((svg) => {
+    svg.remove();
+  });
+  // 删除所有的<img>标签
+  document.querySelectorAll('picture').forEach((img) => {
+    img.remove();
+  });
+  // 删除以Frame_top_2ybWw开头的class
+  document.querySelectorAll(`[class^="Frame_top_"]`).forEach((frame) => {
+    frame.remove();
+  });
+  // 删除以SecBar_secBar_开头的class
+  document.querySelectorAll(`[class^="SecBar_secBar_"]`).forEach((secBar) => {
+    secBar.remove();
+  });
+  // 删除以SecBar_visable_开头的class
+  document.querySelectorAll(`[class^="SecBar_visable_"]`).forEach((visable) => {
+    visable.remove();
+  });
+  // class="Frame_side_3G0Bf grayTheme" 删除这个div
+  document.querySelectorAll(`[class^="Frame_side_"]`).forEach((side) => {
+    side.remove();
+  });
+
+  // Main_side_
+  document.querySelectorAll(`[class^="Main_side_"]`).forEach((side) => {
+    side.remove();
+  });
+  // class="picture picture-box_row_30Iwo"
+  document.querySelectorAll('.picture[class*="picture-box_row_"]').forEach((side) => {
+    side.remove();
+  });
+  // card-video_videoBox_
+  document.querySelectorAll(`[class^="card-video_videoBox_"]`).forEach((side) => {
+    side.remove();
+  });
+  // woo-picture-main
+  document.querySelectorAll('.woo-picture-main').forEach((side) => {
+    side.remove();
+  });
+  // woo-button-main
+  document.querySelectorAll('.woo-button-main').forEach((side) => {
+    side.remove();
+  });
+}
+
+
 export default defineContentScript({
-  matches: ['*://*.google.com/*'],
-  main() {
-    console.log('Hello content.');
+  matches: ['*://*.weibo.com/*'],
+  cssInjectionMode: 'ui',
+  async main(ctx) {
+    console.log('222', 222)
+    // 3. Define your UI
+    const ui = await createShadowRootUi(ctx, {
+      name: 'example-ui',
+      position: 'overlay',
+      anchor: 'html',
+      onMount(container) {
+        const button = document.createElement('button');
+        button.textContent = 'Hello world!';
+        button.classList.add('floating-button');
+        container.append(button);
+        button.addEventListener('click', () => {
+          xxxx()
+        });
+      },
+    });
+
+    // 4. Mount the UI
+    ui.mount();
   },
 });
